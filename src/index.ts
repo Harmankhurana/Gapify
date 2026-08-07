e55966407792820009ae73e59420049f9f112878
Harman khurana <work.harmankhurana@gmail.com>
2026-08-08 04:06:30 +0530

    feat: setup express server with auth routes and mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,16 @@
-let x: number = 10;
-console.log(x);
+import express from "express";
+import dotenv from 'dotenv';
+import { authRouter } from "./routes/auth.routes.js";
+import mongoose from "mongoose";
+import { log } from "node:console";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('api/v1/auth', authRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+    
+})
