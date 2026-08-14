5994d68994872a7999113dde9ade13125775b315
Harman khurana <work.harmankhurana@gmail.com>
2026-08-15 02:49:51 +0530

    feat: add User model and schema

---
+++ src/models/User.ts
@@ -0,0 +1,23 @@
+import { Schema, model, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true, 
+        select: false },
+  },
+  { timestamps: true }
+);
+
+export default model<IUser>('User', userSchema);
