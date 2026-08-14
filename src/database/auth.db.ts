2eb1025233f2a4371aef95cc65a1af6ff7e332c7
Harman khurana <work.harmankhurana@gmail.com>
2026-08-14 20:30:33 +0530

    feat(auth db): Creating an authentication db

---
+++ src/database/auth.db.ts
@@ -0,0 +1,27 @@
+import mongoose, { model, Schema } from "mongoose";
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+const UserModel = model('User', userSchema);
+
+export {
+    UserModel
+}
